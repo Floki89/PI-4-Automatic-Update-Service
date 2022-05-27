e53a3878c21a7b298563b4f8167434be99cdc946
Floki89 <91058911+Floki89@users.noreply.github.com>
2022-05-27 17:45:57 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,13 @@
-#!bin/sh
-sudo apt install update -y
-sudo apt install full-upgrade -y
-sudo apt instal rpi-update
-sudo apt autoclean -y
+#!/bin/sh
+sudo apt-get update && sudo apt-get upgrade -y 
+sudo rpi-update -y
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
+sleep 5
+echo zenity --warning \
+       --title "Wallpen Update" \
+       --width 500 \
+       --height 500 \
+       --text "Update Erfolgreich"
+sleep 5
+shutdown
