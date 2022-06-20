6febefefb7e1da964348f25e6019ba2d5d84a78a
Floki89 <91058911+Floki89@users.noreply.github.com>
2022-06-20 09:19:30 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ sudo apt-get autoremove -y
 sudo apt-get autoclean -y
 sleep 5
 echo zenity --warning \
-       --title "Wallpen Update" \
+       --title "Pi Update" \
        --width 500 \
        --height 500 \
        --text "Update Erfolgreich"
